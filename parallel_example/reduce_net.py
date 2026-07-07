d683240e4148bdf5b40832a08012636d12a4d4d2
Dara <daracheng.hi@gmail.com>
2026-07-07 10:40:38 -0500

    added par

--- parallel_example/reduce_net.py
+++ parallel_example/reduce_net.py
@@ -11,11 +11,8 @@ from DRG_Par import DRG_p
 
 HERE = Path(__file__).resolve().parent
 REPO_ROOT = HERE.parent
-SAVE_DIR =  REPO_ROOT.parent/ "reduced_networks" 
-SAVE_DIR.mkdir(parents=True, exist_ok=True)
-
 NETWORK_PATH    = REPO_ROOT/ "networks" / "kida.uva.2024" / "gas_reactions_kida.uva.2024.in"
-CLUSTER_PATH = REPO_ROOT.parent/ "cluster_0000.npy"
+CLUSTER_DIR = Path("/work/10864/arjunveejay/mysharedirectory/clusters_params_only")
 
 #LOAD NETWORK
 net = Network(grains = True)
@@ -23,19 +20,22 @@ net.load_from_disk(str(NETWORK_PATH))
 dropped = net.drop_passive_species()
 net._get_stoich(dropped) #adds stoichiometric coefficient dictionary to each reaction
 
-#Load Cluster
-cluster = np.load(CLUSTER_PATH)
-cluster = cluster[:100, :]
-
 #Define Source Species and Tolerance
+clust = [f"{num:04}" for num in range(40,50)]
 sources = ['CO', 'C+', 'O+', 'O', 'e-']
-eps = [0.001]
+eps = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2]
 
-#Reduce Network (reduced networks saved to SAVE_DIR under name "reduced_net_eps{eps}.json")
-#if running cluster in batches set prevfolder to directory where previous batch of reduced networks are saved
-drg = DRG_p()
-start = time.perf_counter()
-drg.reduce_net(net, cluster, sources, eps, dropped) 
-end = time.perf_counter()
-print(f"Time {end -start} seconds")
+for c in clust:
+    CLUSTER_PATH = CLUSTER_DIR/f"cluster_{c}.npy"
+    SAVE_DIR =  REPO_ROOT/ "reduced_networks"/c
+    SAVE_DIR.mkdir(parents=True, exist_ok=True)
+
+    cluster = np.load(CLUSTER_PATH)
+    print(f"\nCluster: {c}")
+
+    drg = DRG_p()
+    start = time.perf_counter()
+    drg.reduce_net(net, cluster, sources, eps, dropped, savedir=SAVE_DIR) 
+    end = time.perf_counter()
+    print(f"Time {end -start} seconds")
 
